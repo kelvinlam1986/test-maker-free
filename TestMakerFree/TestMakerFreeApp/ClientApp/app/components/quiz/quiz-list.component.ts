65ee6d9ad8dc2c7664ae5716dd45ed2ae71a0266
Minh Lam <kelvincoder@gmail.com>
2018-09-22 17:36:56 +0700

    Implement sample router in angular 5

--- TestMakerFree/TestMakerFreeApp/ClientApp/app/components/quiz/quiz-list.component.ts
+++ TestMakerFree/TestMakerFreeApp/ClientApp/app/components/quiz/quiz-list.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, Inject, Input, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http"
+import { HttpClient } from "@angular/common/http";
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'quiz-list',
@@ -14,12 +15,14 @@ export class QuizListComponent implements OnInit {
     quizzes: Quiz[];
   
     constructor(private http: HttpClient,
-        @Inject('BASE_URL') private baseUrl: String) {
+        @Inject('BASE_URL') private baseUrl: String,
+        private router: Router) {
     }
 
     onSelect(quiz: Quiz) {
         this.selectedQuiz = quiz;
         console.log('quiz with id ', this.selectedQuiz.Id, ' has been selected');
+        this.router.navigate(["quiz", this.selectedQuiz.Id]);
     }
 
     ngOnInit(): void {
--- TestMakerFree/TestMakerFreeApp/ClientApp/app/components/quiz/quiz.component.ts
+++ TestMakerFree/TestMakerFreeApp/ClientApp/app/components/quiz/quiz.component.ts
@@ -1,4 +1,6 @@
-﻿import { Input, Component } from '@angular/core'
+﻿import { Input, Component, Inject } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: "quiz",
@@ -6,5 +8,28 @@
     styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent {
-    @Input() quiz: Quiz;
+    quiz: Quiz;
+
+    constructor(private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private http: HttpClient,
+        @Inject('BASE_URL') private baseUrl: string) {
+
+        // create empty object from Quiz interface
+        this.quiz = <Quiz>{};
+
+        var id = +this.activatedRoute.snapshot.params["id"];
+        console.log(id);
+
+        if (id) {
+            var url = this.baseUrl + "api/quiz/" + id;
+            this.http.get<Quiz>(url).subscribe(result => {
+                this.quiz = result
+            }, error => console.log(error));
+        } else {
+            console.log("Invalid id: routing back to the home");
+            this.router.navigate(["home"]);
+        }
+    }
+
 }
